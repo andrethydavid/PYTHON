a63ba045373444e92e4d8cc39d4dab0bedddfc32
Andrethy  David Aguilar Ariza <72534486+andrethydavid@users.noreply.github.com>
2023-03-14 22:41:30 -0500

    Add files via upload

---
+++ Poryecto_01.py
@@ -0,0 +1,49 @@
+# Print 
+
+print(12 + 12)
+print(40 -12)
+print(2 * 3)
+print(12 / 3)
+
+print(10 / 2)
+print(10 % 2)
+print(10 / 3)
+print(10 % 3)
+
+print(2 ** 3)
+# Variable 
+
+# una variable es una "caja" o lugar donde puedo almacenar objetivos: numeros,texto;etc
+
+# Las variables poseen un nombre llmado: Identacion
+# es como ponerle un nombre  las cajas con alguna etiqueta o marcado 
+
+# string
+my_name = "Nicolas"
+print(my_name)
+print(type(my_name))
+
+# int
+my_age = 12
+print(my_age)
+print(type(my_age))
+
+# boolean
+
+# son todos los valores verdaderos y falsos  
+
+
+is_single = False
+print(is_single)
+print(type(is_single))
+
+
+# Inputs
+# realizar una espera 
+my_name = input('¿Cuál es tu nombre? ')
+my_age = input('¿Cuántos años tienes? ')
+print(type(my_name))
+print(type(my_age))
+
+
+
