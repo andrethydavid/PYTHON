761fa9af2677b0e034baacb2c9f2fb582d5f4564
Andrethy  David Aguilar Ariza <72534486+andrethydavid@users.noreply.github.com>
2023-03-19 18:30:07 -0500

    Add files via upload

---
+++ Booleans.py
@@ -0,0 +1,15 @@
+# proyecto de booleans
+# los booleanos son todos los docuemntos verdedor y falsos 
+
+
+is_single = True
+print(type(is_single))
+is_single = False
+print(is_single)
+
+
+print(not True)
+print(not False)
+
+is_single = not is_single
+print(is_single)
