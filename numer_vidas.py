b2171d23082f467d475131e5166e6b917b362c0a
Andrethy  David Aguilar Ariza <72534486+andrethydavid@users.noreply.github.com>
2023-03-18 22:30:44 -0500

    Add files via upload

---
+++ numer_vidas.py
@@ -0,0 +1,37 @@
+# proyecto que maneja numeros de un presupuesto 
+
+lives = 3
+print(type(lives))
+age = 12
+buget = 100
+
+# proyecto  de numeros en datos numericos 
+
+temperature = 12.12
+print(type(temperature))
+
+
+#proyecto numerico en vidas de un video juego 
+
+lives = 2
+print(lives)
+lives = 1
+print(lives)
+
+lives = 12 + 15
+print(lives)
+
+lives = lives - 1
+print
+
+lives -= 5
+print(lives)
+
+lives += 5
+print(lives)
+
+number = 450000000000000000000000.1
+print(number)
+
+number_b = 0.00000000000002
+print(number_b)
