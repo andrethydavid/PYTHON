5bdea588b4779340cdd42aa5453697b824d37b46
Andrethy  David Aguilar Ariza <72534486+andrethydavid@users.noreply.github.com>
2023-03-20 20:50:09 -0500

    Add files via upload

---
+++ Transformacion_de_datos.py
@@ -0,0 +1,22 @@
+name = 'david'
+print(type(name))
+name = 12
+print(type(name))
+name = True 
+print(type(name))
+
+
+print("david" + "aguilar")
+print(10+20)
+print("david" +"12")
+
+age = 12
+print("mi edad es" + str (age))
+print(f"mi edad es {age}")
+
+
+age = input('escribe tu edad =>')
+print(type(age))
+age = init(age)
+age += 10
+print(f'tu edad en 10 años sera {age}')
