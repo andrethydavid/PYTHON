db1b719721a6fb30a371c18baf9adbee00c884c4
Andrethy  David Aguilar Ariza <72534486+andrethydavid@users.noreply.github.com>
2023-03-21 23:39:36 -0500

    Add files via upload

---
+++ operador_matematico.py
@@ -0,0 +1,22 @@
+# Operations matematicas 
+print(10+10) # (+) Suma
+print(10-5) # (-) Resta
+print(10*15) # (*) Multiplicación
+print(10/4) #  (/) División
+print(10 % 2) # (%) Modulo o Residuo
+print(10 /3 ) # (//) Division con valor entero
+print(10// 3) 
+
+
+print(2 ** 3 ) # (**) Exponenciación
+print(2**3 + 3 - 7 / 1 // 4)
+print(2**3)
+print((7/1) //4)
+
+print(8+3-1)
+
+
+
+
+print('hola' + 'mundo')
+print('hola '* 3)
