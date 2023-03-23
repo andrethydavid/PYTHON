1887c6c61b8830a4e720e6024dbcf68f354f4ab5
Andrethy  David Aguilar Ariza <72534486+andrethydavid@users.noreply.github.com>
2023-03-22 23:12:06 -0500

    Add files via upload

---
+++ comparacion.py
@@ -0,0 +1,38 @@
+# > mayor q
+print(7 >3)
+print(3 > 7)
+print(7<7)
+
+# <
+print(5< 7)
+print (6<5)
+print(5<5)
+
+
+# >= mayor o igual 
+print(2 >= 1)
+print(2 >= 3)
+print(2 >=2)
+
+# <= 
+print(1 <= 2)
+print(2<=1)
+print(2<=2)
+
+# == igualdad
+
+print (6==6)
+print(5==2)
+print(5 == 7)
+
+# != diferente al numero 
+print(6 != 10)
+print(6 != 6)
+
+# strings == igual 
+print("Apple" == 'Apple')
+print("Apple" == 'apple')
+print("1" == 1 )
+
+age = 15
+print(age >= 18)
