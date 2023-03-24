da1f92bad89aeaa112a37eba52f5d387ee31d578
Andrethy  David Aguilar Ariza <72534486+andrethydavid@users.noreply.github.com>
2023-03-24 17:04:19 -0500

    Add files via upload

---
+++ and.py
@@ -0,0 +1,25 @@
+# and 
+print('AND')
+print('True and True =>', True and True)
+print('True and false =>',True and False)
+print('False and True =>', False and True)
+print('False and False =>', False and False)
+
+
+print(10 > 5 and 5 <10)
+print(10 > 5 and 5 >10)
+
+stock = input('ingrese el numero de stock =>')
+stock = int(stock)
+
+print(stock >= 100 and stock <= 1000 )
+
+print('OR')
+print('True or True =>', True and True)
+print('True or false =>',True and False)
+print('False or True =>', False and True)
+print('False or False =>', False and False)
+
+role = input('digita el rol =>')
+
+print(role == 'admin 'or role == 'seller')
