f631d1adf4e6aab51fdf1065892f873f7b6fb99a
Andrethy  David Aguilar Ariza <72534486+andrethydavid@users.noreply.github.com>
2023-03-24 18:06:54 -0500

    Add files via upload

---
+++ not.py
@@ -0,0 +1,38 @@
+# not
+print(not True)
+print(not False)
+
+# and 
+print('AND')
+print('not True and True =>', not (True and True))
+print('not True and false =>', not (True and False))
+print('not False and True =>', not (False and True))
+print('False and False =>', not (False and False))
+
+
+stock = input('ingrese el numero de stock =>')
+stock = int(stock)
+
+print (not(stock >= 100 and stock <= 1000))
+
+# and table 
+
+# True and True = True
+# True and False =  False
+# False and True = False
+# False and True = False
+
+
+# or # table
+
+#True or True  = True 
+# True or False =  True
+# False or True = True
+# False or False = False
+
+
+# not table 
+
+# not True = False
+# False = True
+
