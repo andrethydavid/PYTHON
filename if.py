21acb7ddfba3e00427bdaf7fff99a127cc1162f2
Andrethy  David Aguilar Ariza <72534486+andrethydavid@users.noreply.github.com>
2023-03-30 22:33:53 -0500

    Add files via upload

---
+++ if.py
@@ -0,0 +1,36 @@
+# condicionales
+# es la estrucura de darle forma al programa con 
+#indicadores if,else,elif
+
+if True:
+  print('deberia ejecutarse')
+
+if False:
+  print('nunca se ejecuta')
+
+  pet = input('¿cual es tu mascota favorita?')
+
+
+if pet ==' perro':
+  print('genial tienes buen gusto')
+
+elif pet == 'gato':
+  print('espero tengas suerte')
+elif pet == 'pez':
+  print('eres lo maximo')
+else:
+  print('no tienes ninguna mascota interesante')
+  
+
+
+'''''
+stock = int(input('Digital el stock =>'))
+
+if stock >= 100 and stock >= 1000:
+  print('el stock es correcto')
+
+else:
+  print('el stock es incorrecto')
+
+'''''
+
