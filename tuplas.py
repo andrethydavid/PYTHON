f014f39a8ea807d7b85e428fec272dd0730200da
Andrethy  David Aguilar Ariza <72534486+andrethydavid@users.noreply.github.com>
2023-04-02 22:48:00 -0500

    Add files via upload

---
+++ tuplas.py
@@ -0,0 +1,10 @@
+number = (1,2,3,4,5)
+strings = ('nico', 'zule','santi')
+print(number)
+print('0 =>', number[0])
+print('-1 =>', number[-1])
+print(type(number))
+
+
+print(strings)
+print(type(strings))
